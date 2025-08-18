efa12a4f4c1e8a7d57fd6b07a1b0400b62887b25
redbluewater <klongnecker@whoi.edu>
2025-08-18 08:07:42 -0700

    finetuning

--- convert.py
+++ convert.py
@@ -1,9 +1,11 @@
 # Krista Longnecker, 13 July 2025
+# Updating 17 August 2025 to fine tune this script
 # Run this after running getBCODMOinfo.ipynb
 # This script will convert the BCO-DMO json file into the format required by CMAP
 # Work on the input for one file, with the end result as one Excel file; will only end up here if the data 
 # file is a CSV file
 # This script works on the discrete data file (the first one I wrote)
+
 
 #some of these are residual from assembling the data file, keep for now.
 import pandas as pd
@@ -55,9 +57,10 @@ def main():
     # time --> CMAP requirement is this: #< Format  %Y-%m-%dT%H:%M:%S,  Time-Zone:  UTC,  example: 2014-02-28T14:25:55 >
     # Do this in two steps so I can check the output more easily
     temp = bcodmo.copy()
-    temp['date'] = pd.to_datetime(temp['decy'], unit='D', origin='1970-01-01')
-    temp['date_cmap'] = temp['date'].dt.strftime("%Y-%m-%dT%H:%M:%S")
-    df['time'] = temp['date_cmap']
+    #you have to change this to a string (.apply(str)) or else this cannot get converted to an Excel variable.
+    temp['date'] = pd.to_datetime(temp['ISO_DateTime_UTC']).apply(str)
+    
+    df['time'] = temp['date']
     
     # lat (-90 to 90) and lon (-180 to 180); use variable names at BCO-DMO
     df['lat'] = bcodmo['Latitude']
@@ -75,9 +78,10 @@ def main():
     templateName = 'datasetTemplate.xlsx'
     sheet_name = 'vars_meta_data'
     vars = pd.read_excel(templateName, sheet_name=sheet_name)
-    cols = vars.columns.tolist()
+    metaVarColumns = vars.columns.tolist()
+    #cols = vars.columns.tolist()
     #df2 will be the dataframe with the metadata about the variables, set it up empty here
-    df2 = pd.DataFrame(columns=cols,index = pd.RangeIndex(start=0,stop=nVariables)) #remember, Python is 0 indexed
+    df2 = pd.DataFrame(columns=metaVarColumns,index = pd.RangeIndex(start=0,stop=nVariables)) #remember, Python is 0 indexed
     
     #the variables I need to search for are here: bcodmo_trim.columns, put them in the first column
     df2['var_short_name'] = bcodmo_trim.columns
@@ -89,39 +93,11 @@ def main():
         a,b = getDetails(md,df2.loc[idx,'var_short_name']) #getDetails is the function I wrote (see above)
         df2.loc[idx,'var_long_name'] = a
         df2.loc[idx,'var_unit'] = b
-    
-    #for sensor I will need a lookup table as the information at BCO-DMO is not formatted to provide this. There is some
-    #sensor information, but I don't see how it is linked directly to specific measured variables, and more than one variable can match a given sensor.
-    #Later note, probably easier to put this into Excel (see notes below about the CMAP keywords that are required)
-    
-    #13 options listed at BCO-DMO:manually assign to one or more variables as possible
-    LUsensors = {'CTD SeaBird 911+':['Temp','CTD_SBE35T','Conductivity','Pressure'],
-                 'Lachat QuikChem 8500 series 2':['NO3_plus_NO2','NO3','NO2','PO4','NH4','SiO2'],
-                 'Shimadzu TOC-V':['DOC'],
-                 'TNM-1 chemiluminescent detector assembly':['TDN'],
-                 'Olympus BX51 epifluorescent microscope':['Bact']
-                }
-    
-    # These other sensors are for data I have not yet tackled, leave here for now
-    # 'MOCNESS'
-    # 'Reeve net'
-    
-    # 'CEC 440HA combustion analyzer'
-    
-    # ##glider, not yet ready, so hold off one these
-    # 'Slocum G2 glbider'
-    # 'WetLabs ECOpuck (ChlF and Bp700)'
-    # 'Submersible Underwater Nitrate Analyzer (SUNA)'
-    # 'Aanderaa O2 optode'
-    
-    
-    # this will return the sensor given a possible variable, surely there is a better way to do this...
-    for idx,item in enumerate(df2.iterrows()):
-        oneVar = df2.loc[idx,'var_short_name']
-        sensor =  str([k for k, v in LUsensors.items() if oneVar in v])[2:-2]
-        if len(sensor): #only try and fill in if a sensor was found
-            df2.loc[idx,'var_sensor'] = str(sensor) #strip off the [] at the beginning/end of the list
-    
+        
+    #these two are easy: just add them here
+    df2.loc[:,('var_spatial_res')] = 'irregular'
+    df2.loc[:, ('var_temporal_res')] = 'irregular'
+       
     #there are a few pieces of metadata that CMAP wants that will be easier to track in an Excel file -
     #make the file once, and then update as needed for future BCO-DMO datasets.
     #The keywords include cruises, and all possible names for a variable. I wonder if
@@ -137,10 +113,23 @@ def main():
     # Discard the columns that acquired a suffix:
     df2 = df2[[c for c in df2.columns if not c.endswith('_td')]]
     
-    #these two are easy: just add them here
-    df2.loc[:,('var_spatial_res')] = 'irregular'
-    df2.loc[:, ('var_temporal_res')] = 'irregular'
 
+    #if moreMD is empty add the details to the CMAP_variableMetdata_additions.xlsx file so I can fill in the information
+    if len(moreMD)==0:
+        with pd.ExcelWriter(fName, engine='openpyxl', mode='a',if_sheet_exists = 'replace') as writer:  
+            df2.to_excel(writer, sheet_name=sheetName,index = False)
+    else:  
+        #otherwise merge the information from moreMD into df2
+        #suffixes are added to column name to keep them separate; '' adds nothing while '_td' adds _td that can get deleted next
+        #update to remove var_sensor as that is now in the Excel file with the metadata details
+        df2 = moreMD.merge(df2[['var_short_name','var_long_name','var_unit']],on='var_short_name',how='left',suffixes=('_td', '',))
+        
+        # Discard the columns that acquired a suffix:
+        df2 = df2[[c for c in df2.columns if not c.endswith('_td')]]
+        #reorder the result to match the expected order        
+        df2 = df2.loc[:,metaVarColumns]
+        
+        
 
     #metadata about the project    
     # finally gather up the dataset_meta_data: for now I just wrote the information here, I might setup in a separate text file later
